b795701269bf352a9189688c3a2167f54aa96538
14likhit <jaijalaram@4495BCU>
2021-07-25 18:52:37 +0530

    - Added Logic to draw shapes.

--- app/src/main/java/com/test/myapplication/ui/task1/Task1Activity.kt
+++ app/src/main/java/com/test/myapplication/ui/task1/Task1Activity.kt
@@ -1,12 +1,29 @@
 package com.test.myapplication.ui.task1
 
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.test.myapplication.R
 import com.test.myapplication.base.BaseActivity
+import com.test.myapplication.databinding.ActivityTask1Binding
 
 class Task1Activity : BaseActivity() {
+
+    private lateinit var dataBinding: ActivityTask1Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_task1)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_task1)
+
+        dataBinding.circleBtn.setOnClickListener {
+            dataBinding.canvasView.drawCircle()
+        }
+
+        dataBinding.squareBtn.setOnClickListener {
+            dataBinding.canvasView.drawSquare()
+        }
+
+        dataBinding.undoBtn.setOnClickListener {
+            dataBinding.canvasView.undo()
+        }
     }
 }
--- app/src/main/java/com/test/myapplication/utils/CanvasView.kt
+++ app/src/main/java/com/test/myapplication/utils/CanvasView.kt
@@ -2,14 +2,98 @@ package com.test.myapplication.utils
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.min
 
-class CanvasView(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : View(
+class CanvasView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(
     context, attrs, defStyleAttr
 ) {
 
+    private val pathList = ArrayList<Path>()
+
+    private var shapeToDraw = 0
+    private val paint = Paint()
+    private val radius = 100F
+    private val length = 100
+    private val random = Random()
+
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        when (shapeToDraw) {
+            0 -> {
+                drawPaths(canvas)
+            }
+            1 -> {
+                drawRandomCircleOnCanvas(canvas)
+            }
+            2 -> {
+                drawRandomSquareOnCanvas(canvas)
+            }
+        }
+    }
+
+    private fun drawRandomCircleOnCanvas(canvas: Canvas?) {
+        val x = random.nextInt(width).toFloat()
+        val y = random.nextInt(height).toFloat()
+        val path = Path()
+        path.addCircle(x, y, radius, Path.Direction.CW)
+        pathList.add(path)
+        drawPaths(canvas)
+    }
+
+    private fun drawRandomSquareOnCanvas(canvas: Canvas?) {
+        val x = random.nextInt(width).toFloat()
+        val y = random.nextInt(height).toFloat()
+        val path = Path()
+        val min = min(x, y)
+        path.addRect(min, min, min + length, min + length, Path.Direction.CW)
+        pathList.add(path)
+        drawPaths(canvas)
+    }
+
+    private fun checkIfIntersect(inputPath: Path): Boolean{
+        var isIntersecting = false
+        for(path in pathList){
+            if(inputPath.op(path,Path.Op.INTERSECT)){
+                isIntersecting = true
+                break
+            }
+        }
+        return isIntersecting
+    }
+
+    private fun drawPaths(canvas: Canvas?) {
+        if (pathList.isNotEmpty()) {
+            for (path in pathList) {
+                canvas?.drawPath(path, paint)
+            }
+        }
+    }
+
+    fun drawCircle() {
+        shapeToDraw = 1
+        invalidate()
+    }
+
+    fun drawSquare() {
+        shapeToDraw = 2
+        invalidate()
+    }
+
+    fun undo() {
+        shapeToDraw = 0
+        if (pathList.isNotEmpty()) {
+            pathList.removeLast()
+        }
+        invalidate()
     }
 }
