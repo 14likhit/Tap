5dc04875d3756a576c44d92b995a67b33eafc1a3
14likhit <jaijalaram@4495BCU>
2021-07-25 15:53:16 +0530

    - Added basic logic to fetch data in Task2Activity.kt

--- app/src/main/java/com/test/myapplication/ui/task2/Task2Activity.kt
+++ app/src/main/java/com/test/myapplication/ui/task2/Task2Activity.kt
@@ -1,12 +1,71 @@
 package com.test.myapplication.ui.task2
 
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.test.myapplication.R
 import com.test.myapplication.base.BaseActivity
+import com.test.myapplication.data.Task2Image
+import com.test.myapplication.data.network.ApiClient
+import com.test.myapplication.data.network.ApiService
+import com.test.myapplication.data.remote.RemoteRepositoryImpl
+import com.test.myapplication.databinding.ActivityTask2Binding
+import com.test.myapplication.ui.task2.adapter.Task2ListAdapter
+import com.test.myapplication.viewmodel.Task2ViewModel
+import com.test.myapplication.viewmodelfactory.Task2ViewModelFactory
+import kotlinx.coroutines.Dispatchers
 
 class Task2Activity : BaseActivity() {
+
+    private lateinit var dataBinding: ActivityTask2Binding
+
+    private lateinit var task2ViewModeFactory: Task2ViewModelFactory
+    private lateinit var task2ViewModel: Task2ViewModel
+
+    private val task2List = ArrayList<Task2Image>()
+    private val task2ListAdapter by lazy { Task2ListAdapter(task2List) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_task2)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_task2)
+
+        task2ViewModeFactory = Task2ViewModelFactory(
+            RemoteRepositoryImpl.getInstance(
+                ApiClient.getRetrofitInstance()!!.create(ApiService::class.java),
+                Dispatchers.IO
+            )!!
+        )
+
+        task2ViewModel =
+            ViewModelProvider(this, task2ViewModeFactory).get(Task2ViewModel::class.java)
+
+        setObservers()
+        setView()
+    }
+
+    private fun setObservers() {
+        task2ViewModel.getImageListLiveData().observe(this, {
+            val imageList = it
+            if (imageList != null) {
+                task2List.addAll(imageList)
+                task2ListAdapter.notifyDataSetChanged()
+            }
+        })
+
+        task2ViewModel.getErrorLiveData().observe(this, {
+
+        })
+
+        task2ViewModel.getLoadingData().observe(this, {
+
+        })
+    }
+
+    private fun setView() {
+        dataBinding.task2ImageListRV.apply {
+            adapter = task2ListAdapter
+        }
+
+        task2ViewModel.getImageListLiveData()
     }
 }
