c27e8a937d92c065ffe390125a0dd2970a2a881d
14likhit <jaijalaram@4495BCU>
2021-07-25 16:14:18 +0530

    - Added Task 1 Activity.

---
+++ app/src/main/java/com/test/myapplication/ui/task1/Task1Activity.kt
@@ -0,0 +1,12 @@
+package com.test.myapplication.ui.task1
+
+import android.os.Bundle
+import com.test.myapplication.R
+import com.test.myapplication.base.BaseActivity
+
+class Task1Activity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task1)
+    }
+}
